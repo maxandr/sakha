01fb5337afcdea70108a3adef174c5504f5721fe
maxandr <maxandryo@gmail.com>
2016-11-10 23:47:13 +0900

    fire

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -11,7 +11,11 @@ namespace UnityStandardAssets._2D
         [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
-
+        public GameObject bullet_instantiate;
+        public GameObject bullet;
+        public float bulletSpeed = 50.0f;
+        public float fireRate = 0.0F;
+        private float nextFire = 0.0F;
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
@@ -29,6 +33,7 @@ namespace UnityStandardAssets._2D
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
             jump_timer = 0.0f;
+			nextFire = Time.time;
         }
 
 
@@ -107,9 +112,37 @@ namespace UnityStandardAssets._2D
 
                 // m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
             }
-                Debug.Log(m_Rigidbody2D.velocity.x);
+            if (Input.GetButton("Fire1") && nextFire <= Time.time)
+            {
+                nextFire = Time.time + fireRate;
+               // var angle = Mathf.Atan2(dir.y, tX * dir.x) * Mathf.Rad2Deg;
+                Fire(Quaternion.Euler(new Vector3(0, 0, 0)), move);
+            }
         }
+        public void Fire(Quaternion pAngle, float move)
+        {
+          //  Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+           // RaycastHit2D hit = Physics2D.Raycast(new Vector2(bullet_instantiate.transform.position.x, bullet_instantiate.transform.position.y), mousePos - new Vector2(bullet_instantiate.transform.position.x, bullet_instantiate.transform.position.y), 100);
+            if (m_FacingRight)
+            {
+                GameObject clone = Instantiate(bullet, bullet_instantiate.transform.position, pAngle/* bullet_instantiate.transform.rotation*/) as GameObject;
+                clone.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+            }
+            else {
+                GameObject clone = Instantiate(bullet, bullet_instantiate.transform.position, pAngle/* bullet_instantiate.transform.rotation*/) as GameObject;
+                clone.transform.Rotate(new Vector3(0  , 180, 0));
+                clone.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed*(-1), 0);
+            }
 
+            // Vector2 a = new Vector2((mousePos.x - gunpoint.transform.position.x), (mousePos.y - gunpoint.transform.position.y));
+            // a.Normalize();
+            //clone.GetComponent<Rigidbody2D>().velocity = a * bulletSpeed;
+            // if (upper_part.GetComponent<WeaponSwapper>().Current != WeaponSwapper.gunTypes.PLAZMAGUN)
+            {
+           //     GameObject sleeve1 = Instantiate(sleeve_prefab, sleeve_spot.transform.position, pAngle/* bullet_instantiate.transform.rotation*/) as GameObject;
+           //     sleeve1.GetComponent<Sleeve_script>().Create(m_FacingRight);
+            }
+        }
 
         private void Flip()
         {
