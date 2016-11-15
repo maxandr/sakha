40263ed556c19c303ff2f93179b909d5e960601e
maxandr <maxandryo@gmail.com>
2016-11-16 03:24:05 +0900

    shooting when crouch

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -5,12 +5,10 @@ using UnityStandardAssets.CrossPlatformInput;
 namespace UnityStandardAssets._2D
 {
     [RequireComponent(typeof (PlatformerCharacter2D))]
-    [RequireComponent(typeof(BoxCollider2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
-        [HideInInspector] public BoxCollider2D coll;
         [HideInInspector] public float axis;
         private void Awake()
         {
