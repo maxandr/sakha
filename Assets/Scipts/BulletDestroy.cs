a51b10459462697ae6177c69e292c237399b1788
maxandr <maxandryo@gmail.com>
2016-11-21 04:00:08 +0900

    Hitting enemies

--- Assets/Scipts/BulletDestroy.cs
+++ Assets/Scipts/BulletDestroy.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletDestroy : MonoBehaviour {
+public class BulletDestroy : MonoBehaviour
+{
     public float death_time;
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         death_time -= Time.deltaTime;
-        if(death_time<=0.0f)
+        if (death_time <= 0.0f)
         {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<Hit>().Hitted();
+        }
+        if (other.gameObject.layer == 20) { 
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scipts/Punch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Punch : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "EnemyHitCollider")
+        {
+            other.gameObject.transform.parent.GetComponent<Hit>().Hitted();
+        }
+    }
+}
